03a5734cd45541f717b318b66f455aed115546b8
Jonathan Disla <jon.disla@gmail.com>
2018-03-12 17:32:19 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+var questions = ["question1", "question2"];
+var correct;
+var incorrect;
+var unanswered;
+
+function start(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+});
